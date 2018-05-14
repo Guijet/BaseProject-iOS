5d4bdfd130c6fcccea5f9400efd3d8913955130a
Guillaume Jetté-Tremblay <guijet2@gmail.com>
2018-05-14 11:11:04 -0400

    Base files commit

---
+++ BaseProject-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaseProject-iOS
+//
+//  Created by Guillaume Jette on 2018-05-14.
+//  Copyright © 2018 Guillaume Jette. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
